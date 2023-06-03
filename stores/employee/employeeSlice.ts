4c44fd8bb23c10c6c9755f4c7944c0bcdd59a81f
hiroki0116 <hirokiseino0116@gmail.com>
2023-06-03 23:24:28 +1000

    Add async redux

--- stores/employee/employeeSlice.ts
+++ stores/employee/employeeSlice.ts
@@ -1,25 +1,59 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
+import { employeeRepository } from "../../api-client/repositories/emloyee_repository";
+import { Employee } from "../../api-client/models/employee";
 
 type EmployeeState = {
   page: number;
+  data: undefined | Employee[];
+  status: string;
 };
 
 const initialState: EmployeeState = {
   page: 1,
+  data: undefined,
+  status: "idle",
 };
+
+export const employeeAsync = createAsyncThunk(
+  "employee/async",
+  async (page: number) => {
+    try {
+      const data = await employeeRepository.getEmployees(page);
+      return {
+        status: "idle",
+        data,
+      };
+    } catch (e) {
+      return {
+        status: "failed",
+        data: [],
+      };
+    }
+  }
+);
 
 export const employeeSlice = createSlice({
   name: "employee",
   initialState,
   reducers: {
-    pagination: (state, action: PayloadAction<number>) => {
+    changePagenation: (state, action: PayloadAction<number>) => {
       state.page += action.payload;
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(employeeAsync.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(employeeAsync.fulfilled, (state, action) => {
+        state.status = "idle";
+        state.data && state.data.push(...action.payload.data);
+      });
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { pagination } = employeeSlice.actions;
+export const { changePagenation } = employeeSlice.actions;
 
 export default employeeSlice.reducer;
